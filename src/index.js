d862bc15360af01611c93ee1f4ffa72a5de89a12
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-03-26 16:04:26 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority")
+  const newTasksList = document.getElementById("tasks");
+  createTaskForm.addEventListener("submit", createNewTask);
+
 });
+
+const createNewTask = function (event) {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("new");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset(); 
+
+const deleteButton = document.createElement("button");
+deleteButton.textContent = "X"
+newTask.appendChild(deleteButton);
+
+deleteButton.addEventListener("click", deleteTask)
+};
+
+const appendNewTask = function (task) {
+  document.getElementById("tasks").appendChild(task);
+}
+
+function deleteTask (event) {
+  event.target.parentNode.remove();
+}
